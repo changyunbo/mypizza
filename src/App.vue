a44b8fd5dc6fbc7ed94df7da07d4b1a0a804b09a
wahaha <chenhengzhi1020@163.com>
2019-03-07 16:23:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="container">
+    <!-- <img src="./assets/logo.png"> -->
+    <Header></Header>
+    <!-- 展示路由 -->
+    <router-view/>
+    <router-view name="history"></router-view>
+    <!-- <router-view name="history"></router-view> -->
+
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+export default {
+  name: 'App',
+  data () {
+    return {
+      img:'../assets/logo.png' 
+      
+    }
+  },
+  components:{
+    'Header':Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/about/contact/Person.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="person">
+    <h3 class="mt-3">{{person}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Person',
+  data () {
+    return {
+      person:'法人代表：web1112全体学生'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
